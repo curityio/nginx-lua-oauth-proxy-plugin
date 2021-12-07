5a82952bc2ab02b11547e2d04c0a24beff3a89ca
gary.archer <gary.archer@WORKMAC.local>
2021-12-07 18:10:02 +0100

    Updates to tests

--- test/test.sh
+++ test/test.sh
@@ -6,23 +6,77 @@
 ##########################################################################################
 
 API_URL='http://localhost:3000'
-ACCESS_TOKEN='1234567890'
-ENCRYPTED_ACCESS_TOKEN='551fbedfde28c548c5b43a8de98c7b59:0fe7776e602953598000d01c5dacd9af'
-ENCRYPTION_KEY='NF65meV>Ls#8GP>;!Cnov)rIPRoK^.NP'
 WEB_ORIGIN='http://www.example.com'
-#export http_proxy='http://127.0.0.1:8888'
+ACCESS_TOKEN='1234567890'
+RESPONSE_FILE=response.txt
+
+#
+# Ensure that we are in the folder containing this script
+#
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #
 # Verify that access is denied for requests without a token or cookie
 #
-#curl $API_URL
+echo '1. Testing OPTIONS request ...'
+HTTP_STATUS=$(curl -i -s -X OPTIONS "$API_URL" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '200' ]; then
+  echo "*** OPTIONS request failed, status: $HTTP_STATUS"
+  exit
+fi
+echo '1. OPTIONS request was handled successfully'
+
+#
+# Verify that access is denied for GET requests without a token or cookie
+#
+echo '2. Testing POST with no credential ...'
+HTTP_STATUS=$(curl -i -s -X POST "$API_URL" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '401' ]; then
+  echo "*** POST wiht no credential failed, status: $HTTP_STATUS"
+  exit
+fi
+echo '2. POST with no credential failed with the expected error'
+JSON=$(tail -n 1 $RESPONSE_FILE)
+echo $JSON | jq
+
+#
+# Verify that cookie requests from an untrusted web origin are rejected
+#
 
 #
 # Verify that an access token sent from a mobile client is passed through to the API
 #
-#curl $API_URL -H "Authorization: Bearer $ACCESS_TOKEN"
+echo '3. Testing POST from mobile client ...'
+HTTP_STATUS=$(curl -i -s -X POST "$API_URL" \
+-H 'Authorization: Bearer 678123egd2huor34' \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '200' ]; then
+  echo "*** POST from mobile client failed, status: $HTTP_STATUS"
+  exit
+fi
+echo '3. POST from mobile client was handled successfully'
+JSON=$(tail -n 1 $RESPONSE_FILE)
+echo $JSON | jq
 
 #
-# Verify that a cookie sent is correctly decrypted
+# Verify that a cookie sent on a GET request is correctly decrypted
 #
-curl $API_URL -H "origin: $WEB_ORIGIN" -H "cookie: example-at=$ENCRYPTED_ACCESS_TOKEN"
+echo '4. Testing GET with a valid encrypted cookie ...'
+ENCRYPTED_ACCESS_TOKEN=$(node utils/encrypt.js "$ACCESS_TOKEN")
+HTTP_STATUS=$(curl -i -s -X GET "$API_URL" \
+-H "origin: $WEB_ORIGIN" \
+-H "cookie: example-at=$ENCRYPTED_ACCESS_TOKEN" \
+-o $RESPONSE_FILE -w '%{http_code}')
+if [ "$HTTP_STATUS" != '200' ]; then
+  echo "*** GET with a valid encrypted cookie failed, status: $HTTP_STATUS"
+  exit
+fi
+echo '4. GET with a valid encrypted cookie was handled successfully'
+JSON=$(tail -n 1 $RESPONSE_FILE)
+echo $JSON | jq
+
+#
+# TODO: POST, OWASP checks etc
+#
