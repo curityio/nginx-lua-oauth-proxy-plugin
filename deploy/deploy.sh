af52ae2da7337235852e305e3d825b6b17220af8
gary.archer <gary.archer@WORKMAC.local>
2021-12-09 13:21:33 +0100

    Use environment variable for encryption key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,6 +19,20 @@ if [ "$PROFILE" != 'openresty' ] && [ "$PROFILE" != 'kong' ]; then
 fi
 
 #
+# Supply the encryption key as an environment variable
+#
+export ENCRYPTION_KEY='NF65meV>Ls#8GP>;!Cnov)rIPRoK^.NP'
+
+#
+# For Kong we must update a template file
+#
+if [ "$PROFILE" == 'kong' ]; then
+  KONG_YML_DATA=$(cat ./kong/kong.template.yml)
+  KONG_YML_DATA=$(sed "s/ENCRYPTION_KEY/$ENCRYPTION_KEY/g" <<< "$KONG_YML_DATA")
+  echo "$KONG_YML_DATA" > ./kong/kong.yml
+fi
+
+#
 # Deploy the system
 #
 docker compose --profile "$PROFILE" --project-name oauthproxy up --build --force-recreate
